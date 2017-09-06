779c3c787bc9c6962118873b21dd50a79fd0ed57
Ed Maphis <emaphis85@gmail.com>
2017-09-05 23:44:10 -0400

    Split off functionality to new module: Hello.hs

---
+++ hello-haskell/src/Hello.hs
@@ -0,0 +1,7 @@
+-- src/Hello.hs
+-- Hello module
+
+module Hello where
+
+sayHello :: IO ()
+sayHello = putStrLn "Hello from Haskell"
--- hello-haskell/src/Main.hs
+++ hello-haskell/src/Main.hs
@@ -1,4 +1,6 @@
 module Main where
 
+import Hello
+
 main :: IO ()
-main = putStrLn "Hello from Haskell!"
+main = sayHello
