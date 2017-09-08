f364885ce96489f9a006abfc4852feabb3b3e9cc
Ed Maphis <emaphis85@gmail.com>
2017-09-07 22:00:24 -0400

    HSpec experiments

--- addition/Addition.hs
+++ addition/Addition.hs
@@ -2,5 +2,12 @@
 
 module Addition where
 
-sayHello :: IO ()
-sayHello = putStrLn "hello!"
+import Test.Hspec
+
+main :: IO ()
+main = hspec $ do
+  describe "Addition" $ do
+    it "1 + 1 is greater than 1" $ do
+      (1 + 1) > 1 `shouldBe` True
+    it "2 + 2 is equal to 4" $ do
+      2 + 2 `shouldBe` 4
---
+++ addition/Math.hs
@@ -0,0 +1,7 @@
+-- | Math.hs
+
+module Math where
+
+absolute :: Int -> Int
+absolute n = if n < 0 then negate n else n 
+
